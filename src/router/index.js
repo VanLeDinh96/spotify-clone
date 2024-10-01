696cb1b5962d82323547032a1a05398f19f89a5e
VanLeDinh96 <dinhvanle.it@gmail.com>
2024-10-01 15:20:47 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SearchView from '../views/SearchView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,11 @@ const router = createRouter({
     {
       path: '/',
       component: HomeView
-    }
+    },
+    {
+      path: '/search',
+      component: SearchView
+    },
   ]
 })
 
